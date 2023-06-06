f49b7699070e4ba6705083c5bec809b07db8a1c8
erichziegler <erich.ziegler11@gmail.com>
2023-06-05 20:50:47 -0600

    Updated App to use Stytch.

--- src/components/appbar/index.js
+++ src/components/appbar/index.js
@@ -1,56 +1,56 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import LoginButton from "../login";
-import LogoutButton from "../logout";
-import Profile from "../profile";
+import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import LogoutButton from '../logout';
+import LoginButton from '../loginButton';
+import Profile from '../profile';
 
 export default function ButtonAppBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flex: 1, textAlign: "center" }}
-            >
-              GameHub
-            </Typography>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <LoginButton />
-            <LogoutButton />
-            <Profile />
-          </div>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position='static'>
+				<Toolbar
+					sx={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+					}}
+				>
+					<div style={{ display: 'flex', alignItems: 'center' }}>
+						<IconButton
+							size='large'
+							edge='start'
+							color='inherit'
+							aria-label='menu'
+							sx={{ mr: 2 }}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography
+							variant='h6'
+							component='div'
+							sx={{ flex: 1, textAlign: 'center' }}
+						>
+							GameHub
+						</Typography>
+					</div>
+					<div
+						style={{
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'center',
+						}}
+					>
+						<LoginButton />
+						<LogoutButton />
+						<Profile />
+					</div>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
 }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
+import { StytchProvider } from '@stytch/react';
+import { StytchUIClient } from '@stytch/vanilla-js';
+
+const stytch = new StytchUIClient(process.env.REACT_APP_STYTCH_PUBLIC_TOKEN);
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-	<Auth0Provider
-	domain='dev-8vljkph5zsws7ttu.us.auth0.com'
-	clientId='XQYYS8TIw6DRoqJPR6IiOWEiUPR1B75t'
-    useRefreshTokens={true}
-    useRefreshTokensFallback={false}
-	authorizationParams={{
-	redirect_uri: window.location.origin,
-	}}
-	>
-	<App />
-	</Auth0Provider>
+	<StytchProvider stytch={stytch}>
+		<App />
+	</StytchProvider>
 );
